fd884ad04c79b17f2f1fc10a6292ade97dc26bc8
tanjirui <351153723@qq.com>
2020-10-29 09:43:52 +0800

    修改数据库密码

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -1,3 +1,5 @@
+create database ssmdemo;
+
 DROP TABLE IF EXISTS `t_user`;  
 CREATE TABLE `t_user` (  
   `user_id` int(11) NOT NULL AUTO_INCREMENT,  
@@ -5,3 +7,6 @@ CREATE TABLE `t_user` (
   `user_age` varchar(20) NOT NULL,  
   PRIMARY KEY (`user_id`)  
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8  ROW_FORMAT=COMPACT;  
+
+
+--访问地址:http://127.0.0.1:8080/ssmdemo/index.jsp
